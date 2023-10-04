9facb74ac75d2e771a3c1c86638e01fcd9af649f
Chatdi <chadi.grolleauraoux@gmail.com>
2023-10-04 16:29:04 +0200

    début navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from "./Components/homepage/homepage.component";
+import {MoviesComponent} from "./Components/movies/movies.component";
+import {TvshowsComponent} from "./Components/tvshows/tvshows.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:"/homepage", pathMatch:"full"},
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'tvshows', component: TvshowsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './Components/sign-in/sign-in.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { MoviesComponent } from './Components/movies/movies.component';
+import { TvshowsComponent } from './Components/tvshows/tvshows.component';
+import { HomepageComponent } from './Components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    NavbarComponent,
+    MoviesComponent,
+    TvshowsComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
