ce2568307d2e57aa4c9deff6ff7c64f29909eb67
Chatdi <chadi.grolleauraoux@gmail.com>
2023-10-06 16:08:28 +0200

    fin de la navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomepageComponent} from "./Components/homepage/homepage.component";
-import {MoviesComponent} from "./Components/movies/movies.component";
-import {TvshowsComponent} from "./Components/tvshows/tvshows.component";
+import {HomepageComponent} from "./Components/Pages/homepage/homepage.component";
+import {MoviesComponent} from "./Components/Pages/movies/movies.component";
+import {TvshowsComponent} from "./Components/Pages/tvshows/tvshows.component";
+import {MyspaceComponent} from "./Components/Pages/myspace/myspace.component";
 
 const routes: Routes = [
   { path: '', redirectTo:"/homepage", pathMatch:"full"},
   { path: 'homepage', component: HomepageComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'tvshows', component: TvshowsComponent },
+  { path: 'myspace', component: MyspaceComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignInComponent } from './Components/sign-in/sign-in.component';
-import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { SignInComponent } from './Components/Authentification/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/Authentification/sign-up/sign-up.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
-import { MoviesComponent } from './Components/movies/movies.component';
-import { TvshowsComponent } from './Components/tvshows/tvshows.component';
-import { HomepageComponent } from './Components/homepage/homepage.component';
+import { MoviesComponent } from './Components/Pages/movies/movies.component';
+import { TvshowsComponent } from './Components/Pages/tvshows/tvshows.component';
+import { HomepageComponent } from './Components/Pages/homepage/homepage.component';
+import { MyspaceComponent } from './Components/Pages/myspace/myspace.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HomepageComponent } from './Components/homepage/homepage.component';
     MoviesComponent,
     TvshowsComponent,
     HomepageComponent,
+    MyspaceComponent,
   ],
   imports: [
     BrowserModule,
