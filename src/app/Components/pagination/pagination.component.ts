8f111352568834ba4d1ad44d1263cd92453884d3
Mohand Arezki <braniciarezki@gmail.com>
2023-11-10 18:20:43 +0100

    Added a pagination functionality to movies and tv shows, there are some bugs to correct in the details of tv shows!

---
+++ src/app/Components/pagination/pagination.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent {
+  @Input() currentPage: number = 0;
+  @Input() totalPages: number = 0;
+  @Output() previous : EventEmitter<void> = new EventEmitter<void>();
+  @Output() next : EventEmitter<void> = new EventEmitter<void>();
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.previous.emit();
+    }
+  }
+  nextPage() : void {
+    if (this.currentPage < this.totalPages) {
+      this.next.emit();
+    }
+  }
+}
+
