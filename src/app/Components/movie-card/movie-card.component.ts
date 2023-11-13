7ef5854a19213f9cfdc4eb41d8e4abb7fd2592ad
Chatdi <chadi.grolleauraoux@gmail.com>
2023-11-13 22:25:25 +0100

    ajout state of movie

--- src/app/Components/movie-card/movie-card.component.ts
+++ src/app/Components/movie-card/movie-card.component.ts
@@ -1,7 +1,8 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Movie, Movies} from "../../Model/movie";
-import {faMagnifyingGlass, faStar} from "@fortawesome/free-solid-svg-icons";
+import {faMagnifyingGlass, faSquareCheck, faStar, faTicket} from "@fortawesome/free-solid-svg-icons";
 import {Router} from "@angular/router";
+
 import {AppComponent} from "../../app.component";
 
 @Component({
@@ -9,28 +10,122 @@ import {AppComponent} from "../../app.component";
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit{
     @Input() movie : Movies | undefined
     @Input() tvShow : Movies | undefined
     @Input() searchResult : Movies | undefined
 
   protected readonly faStar = faStar;
-  isFavoriteClass = 'icon-favorite';
+  protected readonly faTicket = faTicket;
+  protected readonly faSquareCheck = faSquareCheck;
+
+  isFavoriteClass = '';
+  isMustSeeClass = '';
+  isSeenClass = '';
   date : string = '';
-  months = ["Jan", "Fév", "Mars", "Avr", "Juin", "Juil", "Août", "Sept", "Oct", "Nov", "Déc", ]
+  months = ["Jan", "Feb", "Mar", "Apr","May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
 
   constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.initializeFromLocalStorage();
+  }
 
-  addToFavorite(movieId: number | undefined){
-      if(this.isFavoriteClass == 'icon-favorite'){
-        this.isFavoriteClass = "icon-not-favorite";
-        localStorage.removeItem(String(movieId));
-      }else{
-        this.isFavoriteClass = "icon-favorite";
-        localStorage.setItem(String(movieId),"favorite");
+  private initializeFromLocalStorage() {
+    // Vérifie si this.movie existe avant d'effectuer des opérations sur ses propriétés
+    if (this.movie) {
+      const movieId = this.movie.id;
+
+      const favorite = localStorage.getItem(String(movieId));
+      if (favorite === "favorite") {
+        this.isFavoriteClass = 'icon-favorite';
+      } else {
+        this.isFavoriteClass = 'icon-not-favorite';
+      }
+
+      const mustSee = localStorage.getItem(String(movieId));
+      if (mustSee === "mustSee") {
+        this.isMustSeeClass = 'icon-favorite';
+      } else {
+        this.isMustSeeClass = 'icon-not-favorite';
+      }
+
+      const seen = localStorage.getItem(String(movieId));
+      if (seen === "seen") {
+        this.isSeenClass = 'icon-favorite';
+      } else {
+        this.isSeenClass = 'icon-not-favorite';
       }
     }
+  }
+
+  addToFavorite(movieId: number | undefined){
+    if(movieId != undefined){
+      if(this.isFavoriteClass == 'icon-favorite'){
+        this.isFavoriteClass = 'icon-not-favorite fa-fade !important';
+        setTimeout(() => {
+          this.isFavoriteClass = "icon-not-favorite";
+          localStorage.removeItem(String(movieId));
+        }, 1000);
+
+      }
+      if(this.isFavoriteClass == 'icon-not-favorite'){
+        this.isFavoriteClass = 'icon-favorite fa-beat !important';
+        this.isSeenClass = "icon-not-favorite";
+        this.isMustSeeClass = "icon-not-favorite";
+        setTimeout(() => {
+          this.isFavoriteClass = "icon-favorite";
+          this.isSeenClass = "icon-not-favorite";
+          this.isMustSeeClass = "icon-not-favorite";
+          localStorage.setItem(String(movieId),"favorite")
+        }, 1000);
+
+      }
+    }
+  }
+
+  addToMustSee(movieId: number | undefined){
+    if(movieId != undefined){
+      if(this.isMustSeeClass == 'icon-favorite'){
+        this.isMustSeeClass = 'icon-not-favorite fa-shake !important'
+        setTimeout(() => {
+          this.isMustSeeClass = "icon-not-favorite";
+          localStorage.removeItem(String(movieId));
+        }, 1000);
+
+      }else {
+        this.isMustSeeClass = 'icon-favorite fa-shake !important'
+        this.isFavoriteClass = "icon-not-favorite";
+        this.isSeenClass = "icon-not-favorite";
+        setTimeout(() => {
+          this.isMustSeeClass = "icon-favorite";
+          this.isFavoriteClass = "icon-not-favorite";
+          this.isSeenClass = "icon-not-favorite";
+          localStorage.setItem(String(movieId),"mustSee")
+        }, 1000);
+      }
+    }
+  }
+
+  addToSeen(movieId: number | undefined){
+    if(this.isSeenClass == 'icon-favorite'){
+      this.isSeenClass = "icon-not-favorite fa-flip";
+      setTimeout(() => {
+        this.isSeenClass = "icon-not-favorite";
+        localStorage.removeItem(String(movieId));
+      }, 1000);
+    }else{
+      this.isSeenClass = "icon-favorite fa-flip";
+      this.isFavoriteClass = "icon-not-favorite";
+      this.isMustSeeClass = "icon-not-favorite";
+      setTimeout(() => {
+        this.isSeenClass = "icon-favorite";
+        this.isFavoriteClass = "icon-not-favorite";
+        this.isMustSeeClass = "icon-not-favorite";
+        localStorage.setItem(String(movieId),"seen")
+      }, 1000);
+    }
+  }
 
   changeFormatDate(ymd: string | undefined):String{
     if(ymd == undefined){
@@ -43,6 +138,5 @@ export class MovieCardComponent {
   navigateToDetail() {
     this.router.navigate(['/movie_details', this.movie?.id]).then();
   }
-
 
 }
--- src/app/Components/movie-details/movie-details.component.ts
+++ src/app/Components/movie-details/movie-details.component.ts
@@ -1,40 +1,132 @@
-import { Component } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
 import {Movie, Movies} from "../../Model/movie";
 import {MovieDetailService} from "../../Service/MovieDetail/movie-detail.service";
-import {faRankingStar, faStar} from "@fortawesome/free-solid-svg-icons";
+import {faRankingStar, faSquareCheck, faStar, faTicket} from "@fortawesome/free-solid-svg-icons";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss'],
 })
-export class MovieDetailsComponent {
-  movieId : String = '';
-  movieDetail? : Movies | undefined | null;
-  movieTitle:String = ''
+export class MovieDetailsComponent{
+  movieId !: String ;
+  movieDetail ?: Movies ;
+  movieDetail$ !: Observable<Movies> ;
+  movieTitle !:String;
+
   protected readonly faStar = faStar;
-  isFavoriteClass = 'icon-favorite';
-  months = ["Jan", "Fév", "Mars", "Avr", "Juin", "Juil", "Août", "Sept", "Oct", "Nov", "Déc", ]
+  protected readonly faRankingStar = faRankingStar;
+  protected readonly faTicket = faTicket;
+  protected readonly faSquareCheck = faSquareCheck;
+
+  isFavoriteClass = '';
+  isMustSeeClass = '';
+  isSeenClass = '';
+  months = ["Jan", "Feb", "Mar", "Apr","May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec",]
+
 
 
   constructor(private route: ActivatedRoute, private movie : MovieDetailService) {
+    this.movieDetail$ =  movie.getDataMovieDetail(this.movieId);
     this.route.params.subscribe(params => {
       this.movieId = params['id'];
       movie?.getDataMovieDetail(this.movieId)?.subscribe(movie => {
         this.movieDetail = movie;
-        console.log(this.movieDetail)
+        this.initializeFromLocalStorage();
       })
     });
   }
 
-  addToFavorite(movieId: number | undefined){
-    if(this.isFavoriteClass == 'icon-favorite'){
-      this.isFavoriteClass = "icon-not-favorite";
-      localStorage.removeItem(String(movieId));
+  private initializeFromLocalStorage() {
+    // Vérifie si this.movie existe avant d'effectuer des opérations sur ses propriétés
+    if (this.movieDetail) {
+      const movieId = this.movieId;
+
+      const favorite = localStorage.getItem(String(movieId));
+
+      if (favorite === "favorite") {
+        this.isFavoriteClass = 'icon-favorite';
+      } else {
+        this.isFavoriteClass = 'icon-not-favorite';
+      }
+
+      const mustSee = localStorage.getItem(String(movieId));
+      if (mustSee === "mustSee") {
+        this.isMustSeeClass = 'icon-favorite';
+      } else {
+        this.isMustSeeClass = 'icon-not-favorite';
+      }
+
+      const seen = localStorage.getItem(String(movieId));
+      if (seen === "seen") {
+        this.isSeenClass = 'icon-favorite';
+      } else {
+        this.isSeenClass = 'icon-not-favorite';
+      }
+    }
+  }
+
+  stateFavorite(movieId: number | undefined){
+    if(movieId != undefined){
+      if(this.isFavoriteClass == 'icon-favorite'){
+        this.isFavoriteClass = 'icon-not-favorite fa-fade';
+        setTimeout(() => {
+          this.isFavoriteClass = "icon-not-favorite";
+          localStorage.removeItem(String(movieId));
+        }, 1000);
+
+      }
+      if(this.isFavoriteClass == 'icon-not-favorite'){
+        this.isFavoriteClass = 'icon-favorite fa-beat';
+        this.isSeenClass = "icon-not-favorite";
+        this.isMustSeeClass = "icon-not-favorite";
+        setTimeout(() => {
+          this.isFavoriteClass = "icon-favorite";
+          localStorage.setItem(String(movieId),"favorite")
+        }, 1000);
+
+      }
+    }
+  }
+
+  stateMustSee(movieId: number | undefined){
+    if(movieId != undefined){
+      if(this.isMustSeeClass == 'icon-favorite'){
+        this.isMustSeeClass = 'icon-not-favorite fa-shake'
+        setTimeout(() => {
+          this.isMustSeeClass = "icon-not-favorite";
+          localStorage.removeItem(String(movieId));
+        }, 1000);
+
+      }else {
+        this.isMustSeeClass = 'icon-favorite fa-shake'
+        this.isFavoriteClass = "icon-not-favorite";
+        this.isSeenClass = "icon-not-favorite";
+        setTimeout(() => {
+          this.isMustSeeClass = "icon-favorite";
+          localStorage.setItem(String(movieId),"mustSee")
+        }, 1000);
+      }
+    }
+  }
+
+  stateSeen(movieId: number | undefined){
+    if(this.isSeenClass == 'icon-favorite'){
+      this.isSeenClass = "icon-not-favorite fa-flip";
+      setTimeout(() => {
+        this.isSeenClass = "icon-not-favorite";
+        localStorage.removeItem(String(movieId));
+      }, 1000);
     }else{
-      this.isFavoriteClass = "icon-favorite";
-      localStorage.setItem(String(movieId),"favorite");
+      this.isSeenClass = "icon-favorite fa-flip";
+      this.isFavoriteClass = "icon-not-favorite";
+      this.isMustSeeClass = "icon-not-favorite";
+      setTimeout(() => {
+        this.isSeenClass = "icon-favorite";
+        localStorage.setItem(String(movieId),"seen")
+      }, 1000);
     }
   }
 
@@ -47,5 +139,6 @@ export class MovieDetailsComponent {
   }
 
 
-  protected readonly faRankingStar = faRankingStar;
+
+
 }
