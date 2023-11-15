b69263f363a113bdee766ee8203e43cbf691a337
Chatdi <chadi.grolleauraoux@gmail.com>
2023-11-15 01:01:44 +0100

    ajout myspace sans style

--- src/app/Components/myspace/myspace.component.ts
+++ src/app/Components/myspace/myspace.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {MovieDetailService} from "../../Service/MovieDetail/movie-detail.service";
+import {Movies} from "../../Model/movie";
+import {TvShow, TvShows} from "../../Model/tvShow";
 
 @Component({
   selector: 'app-myspace',
@@ -7,41 +10,119 @@ import {Router} from "@angular/router";
   styleUrls: ['./myspace.component.scss']
 })
 export class MyspaceComponent implements OnInit{
-  favorisFromLocalStorage: number[] = [];
-  seenFromLocalStorage: number[] = [];
-  mustSeeFromLocalStorage: number[] = [];
+  favoriteFromLocalStorage: String[] = [];
+  seenFromLocalStorage: String[] = [];
+  mustSeeFromLocalStorage: String[] = [];
 
-  constructor(private router: Router) { }
+  favoriteMovie: Movies[] = [];
+  favoriteTvShows: TvShows[] = [];
+
+  mustSeeMovie: Movies[] = [];
+  mustSeeTvShows: TvShows[] = [];
+
+  seenMovie: Movies[] = [];
+  seenTvShows: TvShows[] = [];
+
+  thisMovie: Movies | undefined;
+  thisTvShow: TvShows | undefined;
+
+  constructor(private router: Router, private service:MovieDetailService) { }
 
   logout() {
-    // Perform logout logic here, e.g., clear user session or token.
-
-    // Redirect to the login page (replace 'login' with your actual login route).
-    this.router.navigate(['/sign_in']);
+    this.router.navigate(['/sign_in']).then(r => true);
   }
-
-
-
   ngOnInit() {
     for (let i = 0; i < localStorage.length; i++) {
       let key:string | null = localStorage.key(i);
 
-
-
-
       if(key != null) {
         if (localStorage.getItem(key) == "favorite") {
-          this.favorisFromLocalStorage.push(parseInt(key));
+          this.favoriteFromLocalStorage.push(key);
         } else if (localStorage.getItem(key) == "seen") {
-          this.seenFromLocalStorage.push(parseInt(key));
-        }
-        if (localStorage.getItem(key) == "mustSee") {
-          this.mustSeeFromLocalStorage.push(parseInt(key));
+          this.seenFromLocalStorage.push(key);
+        }else if (localStorage.getItem(key) == "mustSee") {
+          this.mustSeeFromLocalStorage.push(key);
         }
       }
     }
-    console.log(this.mustSeeFromLocalStorage)
-    console.log(this.favorisFromLocalStorage)
-    console.log(this.seenFromLocalStorage)
+    for (let f=0; f < this.favoriteFromLocalStorage.length; f++){
+      let keyFav = this.favoriteFromLocalStorage[f];
+      if(keyFav != null) {
+        if(keyFav.split('M')[0] != keyFav){
+          this.service.getDataMovieDetail(keyFav.split('M')[0])
+            .subscribe(movie => {
+              this.favoriteMovie.push(movie);
+            });
+        }else if(keyFav.split('TVS')[0] != keyFav){
+          this.service.getDataTvShowsDetail(keyFav.split('TVS')[0])
+            .subscribe(tvShow => {
+              this.favoriteTvShows.push(tvShow);
+            });
+        }
+      }
+    }
+
+    for (let ms=0; ms < this.mustSeeFromLocalStorage.length; ms++){
+      let keyMS = this.mustSeeFromLocalStorage[ms];
+      if(keyMS != null) {
+        if(keyMS.split('M')[0] != keyMS){
+          this.service.getDataMovieDetail(keyMS.split('M')[0])
+            .subscribe(movie => {
+              this.mustSeeMovie.push(movie);
+            });
+        }else if(keyMS.split('TVS')[0] != keyMS){
+          this.service.getDataTvShowsDetail(keyMS.split('TVS')[0])
+            .subscribe(tvShow => {
+              this.mustSeeTvShows.push(tvShow);
+            });
+        }
+      }
+    }
+
+    for (let s=0; s < this.seenFromLocalStorage.length; s++){
+      console.log(this.seenFromLocalStorage)
+
+      let keyS = this.seenFromLocalStorage[s];
+      console.log(keyS)
+      if(keyS != null) {
+        if(keyS.split('M')[0] != keyS){
+          this.service.getDataMovieDetail(keyS.split('M')[0])
+            .subscribe(movie => {
+              this.seenMovie.push(movie);
+            });
+        }else if(keyS.split('TVS')[0] != keyS){
+          this.service.getDataTvShowsDetail(keyS.split('TVS')[0])
+            .subscribe(tvShow => {
+              this.seenTvShows.push(tvShow);
+            });
+        }
+      }
+    }
   }
+
+  protected getMovieOf(id:String):Movies | undefined{
+    if((id=id.split('M')[0]) == id){
+      this.service.getDataMovieDetail(id)
+        .subscribe(movie => {
+          this.thisMovie = movie;
+        });
+    }
+    if(this.thisMovie)
+      return this.thisMovie;
+    return undefined;
+  }
+
+  protected getTvShowOf(id:String):TvShows | undefined{
+    if((id=id.split('TVS')[0]) == id){
+      this.service.getDataTvShowsDetail(id)
+        .subscribe(tvShow => {
+          this.thisTvShow = tvShow;
+        });
+    }
+    if(this.thisTvShow)
+      return this.thisTvShow;
+    return undefined;
+  }
+
+
 }
